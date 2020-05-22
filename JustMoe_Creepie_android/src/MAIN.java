10a563df96c8b9a2cfcafe40b40710cd76e540d6
JustMoe <62350723+JustMoe@users.noreply.github.com>
2020-05-22 15:20:57 +0200

    added some awesome code to our awesome project

--- JustMoe_Creepie_android/src/MAIN.java
+++ JustMoe_Creepie_android/src/MAIN.java
@@ -4,6 +4,10 @@ public class MAIN {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		for (int i=0;i<10;i++) {
+			System.out.println("Hola Mario");
+		}
 	}
 
 }
