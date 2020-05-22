740e1877b04198aac4028b93db18cd3ebcb5835b
MarioEberth <marioeberth@gmail.com>
2020-05-22 15:36:25 +0200

    write some nice code

--- JustMoe_Creepie_android/src/MAIN.java
+++ JustMoe_Creepie_android/src/MAIN.java
@@ -8,10 +8,12 @@ public class MAIN {
 		for (int i=0;i<10;i++) {
 			System.out.println("Hola Mario");
 		}
+
 		
 	}
 	
 	public void thisIsANewMethod() { 
+		System.out.println("Free like a bird in a box in a dark room ");
 		
 	}
 
