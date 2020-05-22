02f5ddb4ca7746c43ca689bee0db14029244a70e
benjaminellmer <s1910237013@students.fh-hagenberg.at>
2020-05-22 17:32:12 +0200

    Please accept
    Thanks!

--- JustMoe_Creepie_android/src/MAIN.java
+++ JustMoe_Creepie_android/src/MAIN.java
@@ -3,7 +3,7 @@ public class MAIN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Benji war hier");
 		
 		for (int i=0;i<10;i++) {
 			System.out.println("Hola Mario");
